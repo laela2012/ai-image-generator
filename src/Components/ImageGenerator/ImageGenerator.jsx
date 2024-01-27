10cd70e8fd70ce540c5bd7a75eb55fa562efde1f
Laela2012 <cute39zlela@yahoo.co.id>
2024-01-27 13:47:35 +0700

    AI image generator like Dall-E or Midjourney - UI looks

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
+
+export const ImageGenerator = () => {
+  return (
+    <div className="ai-image-generator">
+        <div className="header">
+            AI Image <span>Generator</span>
+        </div>
+        <div className="img-loading">
+            <div className="image">
+                <img src={default_image} alt="" />
+            </div>
+        </div>
+        <div className="search-box">
+            <input type="text" className='search-input' placeholder='Describe what you want to see' />
+            <div className="generate-btn">Generate</div>
+        </div>
+    </div>
+  )
+}
